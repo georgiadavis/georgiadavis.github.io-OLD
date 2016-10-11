dd8b42d5b050a348eb8e13dd7435c26ef8c95045
Georgia Davis <georgiadavis@Georgias-MacBook-Pro.local>
2016-10-11 15:15:05 -0700

    fix formatting

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 (function () {
     var responseContainer = document.querySelector('#response-container');
     var haikus = [
-        ' I go on fun hikes 🗻 \n and ride my bright orange bike 🚴🏼 \n it is a delight 😁 \n',
-        ' I love to eat fruit 🍑 \n and some delicious ice cream 🍦 \n on a sunny day ☀️ \n',
-        ' I live in San Fran 🌉 \n It is too hipster for me 🚶\n But I like the fog ☁️ \n',
-        ' I like boba tea 🍵 \n It is so great when its free 💵 \n my friend pays for me 🙋🏻 \n',
-        ' I am a dancer 💃🏻 \n and a former school teacher 🍎\n and a designer ✏️\n'
+        'I go on fun hikes 🗻 \nand ride my bright orange bike 🚴🏼 \nit is a delight 😁 \n',
+        'I love to eat fruit 🍑 \nand some delicious ice cream 🍦 \non a sunny day ☀️ \n',
+        'I live in San Fran 🌉 \nIt is too hipster for me 🚶\nBut I like the fog ☁️ \n',
+        'I like boba tea 🍵 \nIt is so great when its free 💵 \nmy friend pays for me 🙋🏻 \n',
+        'I am a dancer 💃🏻 \nand a former school teacher 🍎\nand a designer ✏️\n'
     ];
     var randomHaiku = haikus[Math.floor(Math.random() * haikus.length)];
 
